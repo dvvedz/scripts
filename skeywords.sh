ed12a365096f5931eda7e9f87322411651151ef1
root <oskar9899@gmail.com>
2024-10-14 17:33:42 +0200

    updated

--- skeywords.sh
+++ skeywords.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 while read -r line; do
-  echo $line | sed -e 's/\./\n/g' -e 's/\-/\n/g' -e 's/[0-9]*//g'
-done < /dev/stdin | sort -u 
+  rmTld="${line%.*}"
+  echo $rmTld | sed -e 's/\./\n/g' -e 's/\-/\n/g' -e 's/[0-9]*//g'
+done < /dev/stdin
 
